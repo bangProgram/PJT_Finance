723b9a9a02c38fd863a096d59c4a99ac0fbae641
bangProgram <40655921+bangProgram@users.noreply.github.com>
2023-11-28 21:58:19 +0900

    20231128 commit
    기업상세 실적표 작업중

--- Finance/src/main/java/finance/app/corporation/service/AppCorporationService.java
+++ Finance/src/main/java/finance/app/corporation/service/AppCorporationService.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Service;
 public interface AppCorporationService {
 	public List<Map<String, Object>> getCorpListYear(Map<String, Object> param) throws Exception;
 	public List<Map<String, Object>> getCorpListHalf(Map<String, Object> param) throws Exception;
+	public List<Map<String, Object>> getCorpPerform(Map<String, Object> param) throws Exception;
 }
