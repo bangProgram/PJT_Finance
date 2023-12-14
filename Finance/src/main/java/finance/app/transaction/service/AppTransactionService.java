938dbe62a2f3556effda7fc5010357415118f621
bangProgram <40655921+bangProgram@users.noreply.github.com>
2023-12-14 17:55:10 +0900

    20231214 commit
    거래일지 작성 작업중

--- Finance/src/main/java/finance/app/transaction/service/AppTransactionService.java
+++ Finance/src/main/java/finance/app/transaction/service/AppTransactionService.java
@@ -6,4 +6,6 @@ import java.util.Map;
 public interface AppTransactionService {
 
 	public List<Map<String, Object>> getTransRecord(Map<String, Object> param) throws Exception;
+
+	public List<Map<String, Object>> getTransCorpList(Map<String, Object> param) throws Exception;
 }
