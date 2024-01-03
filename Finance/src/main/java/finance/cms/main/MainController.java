2c810330e3e0f8ec7f958e5f7a46dfbcbd066228
bangProgram <40655921+bangProgram@users.noreply.github.com>
2024-01-03 20:48:07 +0900

    20230103 commit
    개별 코딩테스트 준비작업

--- Finance/src/main/java/finance/cms/main/MainController.java
+++ Finance/src/main/java/finance/cms/main/MainController.java
@@ -1,7 +1,14 @@
 package finance.cms.main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -59,5 +66,43 @@ public class MainController extends DefaultController{
 	@RequestMapping(value={"/main/privacyPolicy"} , method = RequestMethod.GET)
 	public ModelAndView goPrivacyPolicy() {
 	    return new ModelAndView("common/privacyPolicy.jsp");
+	}
+	
+	@RequestMapping(value={"/test"} , method = RequestMethod.GET)
+	public ModelAndView test(HttpServletRequest request, HttpServletResponse response, @RequestParam Map<String, Object> commandMap) throws Exception{
+		try {
+			System.out.println("입력하세요 >> ");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			Scanner sc = new Scanner(System.in);
+			int cnt, min = 0, max = 0;
+			cnt = Integer.parseInt(br.readLine());
+			String[] numList = br.readLine().split(" ");
+			for(int i=0; i<cnt; i++) {
+				int num = Integer.parseInt(numList[i]);
+				if(i == 0) {
+					min = num;
+					max = num;
+				}else {
+					if(num > max) {
+						max = num;
+					}
+					if(num < min) {
+						min = num;
+					}
+				}
+			}
+			System.out.println(min+" "+max);
+			sc.close();
+		}
+		catch (Exception e) {
+			System.out.println("error : IOexception");
+		}
+		
+		ModelAndView mav = new ModelAndView();
+		String resultUrl = "main/test.jsp";
+		mav.setViewName(resultUrl);
+		
+		return mav;
+		
 	}
 }
