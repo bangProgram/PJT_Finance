67902cd259c0986a674543448cd8113f8711c4a7
bangProgram <40655921+bangProgram@users.noreply.github.com>
2024-01-04 12:23:18 +0900

    commit
    dd

---
+++ Finance/src/main/java/finance/common/Controller/CommonConstants.java
@@ -0,0 +1,13 @@
+package finance.common.Controller;
+
+public interface CommonConstants {
+
+	public static final String SESSION_MEMBER = "memberVO";
+	
+	//KIS DEV 한국투자증권 Key
+	public static final String kisDeveloperAppKey = "PSxf3FCyBL6v5OjguQoxK7LglkJujUSpAeIG";
+	public static final String kisDeveloperAppSecretKey = "ajHW/udInbXs5u/4V3IGhilCTMWdHfY3eRa06uSbSMT+sA/nWmFkeoXX5vQgIrBzkBwJotsrgN+o2rnLAd/Tk63B8Y4m60oHxFJryF48vmIvGvl1EWNn+vlkr4fAk8xZ2ZDi6jT2CBcomS/Aym+1sSgDiP0Rmxpmd+SOd4EdQU20QKZC8vA=";
+	
+	// DART DEV 전자공시 Key
+	public static final String openDartCertifiedKey = "fb1e1e5223c66ce1175f545ddd0ea9a15984528a";
+}
